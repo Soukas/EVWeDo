d61040ae32db3c0ea734f5fd6ce01e70550ebb4e
Soukas <cgsoukas@gmail.com>
2017-08-05 14:45:20 -0400

    fixed sync issue

--- sync.bash
+++ sync.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 git pull
 chown www-data:www-data /var/www/evwedo.com/images/*.*
-chmod 770 /var/www/evwedo.com/images*.*
+chmod 770 /var/www/evwedo.com/images/*.*
 chown www-data:www-data /var/www/evwedo.com/public_html/*.*
 chown www-data:www-data /var/www/evwedo.com/*.*
 chmod 770 /var/www/evwedo.com/*.*
