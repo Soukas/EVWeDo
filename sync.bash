ccdc58753ba681edc7b60adf7b0c825391cdb0f4
CGSoukas <CGSoukas@gmail.com>
2017-08-05 14:17:46 -0400

    added sync file for permissions

---
+++ sync.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+chown www-data:www-data /var/www/evwedo.com/public_html/*.*
+chown www-data:www-data /var/www/evwedo.com/*.*
+chmod 770 /var/www/evwedo.com/*.*
+chmod 770 /var/www/evwedo.com/public_html/*.*
